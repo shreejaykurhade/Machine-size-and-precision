ec73e754a14635755f008727a41ed8806b6a761d
Shreejay Kurhade <93570022+shreejaykurhade@users.noreply.github.com>
2024-05-30 12:07:35 +0530

    Add files via upload

---
+++ machine_size_and_precision.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+int main(){
+
+cout<<"sizeof information"<<endl;
+cout<<"=================================="<<endl;
+
+cout<<"char : "<<sizeof(char)<<" bytes"<<endl;
+cout<<"int : "<<sizeof(int)<<" bytes"<<endl;
+cout<<"unsigned int :"<<sizeof(unsigned int)<<" bytes"<<endl;
+cout<<"short : "<<sizeof(short)<<" bytes"<<endl;
+cout<<"long : "<<sizeof(long)<<" bytes"<<endl;
+cout<<"long long : "<<sizeof(long long)<<" bytes"<<endl;
+
+cout<<"=================================="<<endl;
+
+cout<<"float : "<<sizeof(float)<<" bytes"<<endl;
+cout<<"double : "<<sizeof(double)<<" bytes"<<endl;
+cout<<"long double : "<<sizeof(long double)<<" bytes"<<endl;
+
+cout<<"=================================="<<endl;
+
+cout<<"Minimum values:"<<endl;
+cout<<"char : "<<CHAR_MIN<<" bytes"<<endl;
+cout<<"int : "<<INT_MIN<<" bytes"<<endl;
+cout<<"short : "<<SHRT_MIN<<" bytes"<<endl;
+cout<<"long : "<<LONG_MIN<<" bytes"<<endl;
+cout<<"long long : "<<LLONG_MIN<<" bytes"<<endl;
+
+cout<<"=================================="<<endl;
+
+cout<<"Maximum values:"<<endl;
+cout<<"char : "<<CHAR_MAX<<" bytes"<<endl;
+cout<<"int : "<<INT_MAX<<" bytes"<<endl;
+cout<<"short : "<<SHRT_MAX<<" bytes"<<endl;
+cout<<"long : "<<LONG_MAX<<" bytes"<<endl;
+cout<<"long long : "<<LLONG_MAX<<" bytes"<<endl;
+
+cout<<"=================================="<<endl;
+
+
+}
